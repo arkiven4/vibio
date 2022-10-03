f40dd5197b8a24889fbe3c66992900104e61b0dc
Pras Kiven <arkiven4@gmail.com>
2022-10-03 09:09:55 +0700

    fix vercel bug

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,23 +1,23 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import Cors from 'cors'
-const cors = Cors({
-  methods: ['POST', 'GET', 'HEAD'],
-})
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import Cors from 'cors'
+// const cors = Cors({
+//   methods: ['POST', 'GET', 'HEAD'],
+// })
 
 
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-      return resolve(result)
-    })
-  })
-}
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
+//       return resolve(result)
+//     })
+//   })
+// }
 
-export default async function handler(req, res) {
-  //req.method === 'POST'
-  await runMiddleware(req, res, cors)
-  res.status(200).json({ name: "John Doe", query: req.query });
-}
+// export default async function handler(req, res) {
+//   //req.method === 'POST'
+//   await runMiddleware(req, res, cors)
+//   res.status(200).json({ name: "John Doe", query: req.query });
+// }
