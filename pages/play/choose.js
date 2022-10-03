372a76bb69cc8bdc035ca35c7aab990530820b97
Pras Kiven <arkiven4@gmail.com>
2022-10-03 09:06:30 +0700

    fix vercel bug

--- pages/play/choose.js
+++ pages/play/choose.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
-import stylesCustom from "../styles/custom.module.css";
+import stylesCustom from "../../styles/custom.module.css";
 
 import { useRouter } from "next/router";
 
